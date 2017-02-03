394aee2764c9dc19e157b22d96ac7bae3a2d9715
gakessle <gakessle@cisco.com>
2017-02-03 14:15:43 -0500

    make it work

--- squares.rb
+++ squares.rb
@@ -1,11 +1,12 @@
+srand
 names = ["Colton", "Steve", "Pavan", "Albert", "Gabe", "David", "Jason", "Blake", "Winfred", "Doug"].shuffle
 numbers = (1..100).to_a
-numbers = numbers.shuffle
 final = {}
-puts numbers
 names.each do |name|
   new_numbers = numbers.sample(10)
+  puts numbers.size
+  puts "**********"
   final = final.merge({name.to_sym => new_numbers})
-  numbers.delete(new_numbers)
+  numbers = numbers - new_numbers
 end
 puts final
